c871efe6f988efa145cadd0778a6a22689348a58
Vardaan <vardaan13121997@gmail.com>
2021-11-07 21:18:14 +0530

    added log

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -87,6 +87,7 @@ exports.webhookCheckout = (req, res, next) => {
             process.env.STRIPE_WEBHOOK_SECRET
         );
     } catch (err) {
+        console.log(err);
         return res.status(400).send(`Webhook error: ${err.message}`);
     }
 
